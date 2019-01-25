7a21d223a4b6f5103312fecf61fa24079aca81f5
cor14095 <ale.crts1996@gmail.com>
2019-01-25 14:25:00 -0600

    Fix typos in comments, fix format to be consistent with PEP 8, fix a minor bug on FormatTime() and change functions from mix to camel.

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import sys, getopt
 
 global_error_stack = []
 
-def openJSON(fileName):
+def OpenJSON(fileName):
     with open('input/' + fileName) as data_file:
         data = json.load(data_file)
     return data
 
-def writeJSON(data, filename):
+def WriteJSON(data, filename):
     # Print the file as a JSON.
     with open('output/output_' + filename, 'w') as outfile:
         json.dump(data, outfile, ensure_ascii=False
             , sort_keys=False, indent=4, separators=(',', ': '))
 
-def formatTimes(stringTime):
+def FormatTime(stringTime):
     # Function input is a string with a format like this 'HH:MM[PM/AM]'.
     # Returns Time in 24h format wo PM or AM.
     result = 0.0
@@ -24,36 +24,43 @@ def formatTimes(stringTime):
     # Check for correct format.
     if (len(timeSplited) != 2):
         # Format is wrong
-        global_error_stack.append("Wrong format for: formatTimes("
-        + stringTime + ").")
+        global_error_stack.append("Wrong format for: FormatTime({})".
+            format(stringTime))
         result = -1.0
         return result
     else:
-        timeSuffix = timeSplited[1][2:]
-        # If no time sufix provided it's assumed to be 24hs format.
-        hours = ((float(timeSplited[0]) + 12) if timeSuffix == "PM"
-            else float(timeSplited[0]))
-        hours = 12.0 if hours == 24.0 else hours
-        minutes = float(timeSplited[1][:2])
-        minutes = (100.0 * minutes) / 6000.0    # divie 60 divide 100.
+        timeSuffix = timeSplited[1][-2:]
+        # If no time sufix provided it's assumed to be 24hrs format.
+        try:
+            hours = ((float(timeSplited[0]) + 12) if timeSuffix == "PM"
+                else float(timeSplited[0]))
+            hours = 12.0 if hours == 24.0 else hours
+            minutes = float(timeSplited[1][:2])
+        except ValueError:
+            global_error_stack.append("Wrong value for: FormatTime({})".
+                format(stringTime))
+            hours = -1.0
+            minutes = -1.0
+
+        minutes = (100.0 * minutes) / 6000.0    # divide 60 divide 100.
 
         result = hours + minutes
 
-        # Check if time makes sence
+        # Check if time makes sense
         if (result < 0.0 or result > 24.0):
-            global_error_stack.append("Wrong value for: formatTimes("
-            + stringTime + ").")
+            global_error_stack.append("Wrong value for: FormatTime({})".
+                format(stringTime))
             result = -1.0
         return result
 
-def deFortmatTime(floatTime):
-    # This function converts output of formatTimes() to original.
+def DeformatTime(floatTime):
+    # This function converts output of FormatTime() to original.
     result = ""
     timeSplited = str(floatTime).split('.');
 
-    timeSuffix = ("PM" if float(timeSplited[0]) > 11
-        else "AM")
-    hours = ((float(timeSplited[0]) - 12) if timeSuffix == "PM"
+    timeSuffix = ('PM' if float(timeSplited[0]) > 11
+        else 'AM')
+    hours = ((float(timeSplited[0]) - 12) if timeSuffix == 'PM'
         else float(timeSplited[0]))
     hours = 12.0 if hours == 0.0 else hours
     minutes = float(timeSplited[1])
@@ -63,18 +70,17 @@ def deFortmatTime(floatTime):
 
     return str(result)
 
-def isEmployeeAvailable(key, employee):
+def IsEmployeeAvailable(key, employee):
     # This function check if the employee is available at that time.
     available = False if (key in employee) else True
-
     return available
 
-def printResults(data, filename):
-    # This function prints the results in a nice way.
+def PrintResults(data, filename):
+    # This function prints the results in a pretty way.
     # To a file as JSON and as a text report.
 
     # Print the file as a JSON.
-    writeJSON(data, filename)
+    WriteJSON(data, filename)
 
     # Generate a nice string:
     text = "Employees availability:\n"
@@ -87,38 +93,39 @@ def printResults(data, filename):
     print (text)
 
     # Write a .txt file with the data.
-    with open("output/output_" + filename.split('.')[0] + ".txt", 'a') as out:
+    with open('output/output_' + filename.split('.')[0] + '.txt', 'a') as out:
         out.write(text + '\n')
 
     print ("Output files printed to:")
     print ("-> output/" + filename + "\t JSON formated file.")
-    print ("-> output/" + filename.split('.')[0] + ".txt" +
-        "\t text formated file.")
+    print ("-> output/" + filename.split('.')[0] + ".txt"
+            + "\t text formated file.")
 
 
 def main(argv):
     inputfile = 'input.json'
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile="])
+        opts, args = getopt.getopt(argv,'hi:o:',['ifile='])
     except getopt.GetoptError:
         print ('main.py -i <inputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':   # -h help command
-            print ('test.py -i <inputfile> \tFor an specific file name.')
-            print ('test.py \tProgram will look for input.json file.')
+            print ("test.py -i <inputfile> \tFor an specific file name.")
+            print ("test.py \tProgram will look for input.json file.")
             sys.exit()
-        elif opt in ("-i", "--ifile"):
+        elif opt in ('-i', '--ifile'):
             inputfile = arg
 
     # Variables
     try:
-        data = openJSON(inputfile)     # Data from json.
+        data = OpenJSON(inputfile)     # Data from json.
     except FileNotFoundError:
-        global_error_stack.append("No file named 'input.json' found.")
+        global_error_stack.append("No file named '{}' found.".
+            format(inputfile))
         return 0
     try:
-        staff = data["staff"]   # Staff schedulesself.
+        staff = data['staff']   # Staff schedulesself.
     except KeyError:
         global_error_stack.append("'staff' key doesn't exist!" +
             " Check README file to see the format.")
@@ -128,17 +135,17 @@ def main(argv):
     # Build a schedule dictionary and fill it with the data.
     # Initialize some important values.
     try:
-        startTime = formatTimes(data["WorkHours"]["start"])
-        endTime = formatTimes(data["WorkHours"]["end"])
-        lunchStart = formatTimes(data["Lunch"]["start"])
-        lunchEnd = formatTimes(data["Lunch"]["end"])
+        startTime = FormatTime(data['WorkHours']['start'])
+        endTime = FormatTime(data['WorkHours']['end'])
+        lunchStart = FormatTime(data['Lunch']['start'])
+        lunchEnd = FormatTime(data['Lunch']['end'])
     except KeyError:
-        global_error_stack.append("One of the main keys or values doesn't" +
-            " exist, check README file to see the format.")
+        global_error_stack.append("One of the main keys or values doesn't"
+            + " exist, check README file to see the format.")
         return 0
 
     try:
-        meetDuration = (float(data["MeetDuration"]) * 100.0) / 6000.0
+        meetDuration = (float(data['MeetDuration']) * 100.0) / 6000.0
     except ValueError:
         global_error_stack.append(
             "Meet duration must be in minutes and in the range of 0 to 60."
@@ -146,8 +153,8 @@ def main(argv):
         )
         meetDuration = -1
     except KeyError:
-        global_error_stack.append("'MeetDuration' key doesn't exist!" +
-            " Check README file to see the format.")
+        global_error_stack.append("'MeetDuration' key doesn't exist!"
+            + " Check README file to see the format.")
         return 0
 
     # Check that all this values are correct.
@@ -155,24 +162,24 @@ def main(argv):
         global_error_stack.append("Wrong value on JSON 'WorkHours' or 'Lunch'.")
         return 0
     elif (lunchStart > lunchEnd or startTime > endTime):
-        global_error_stack.append("Start times must be lower than end times " +
-            "for WorkHours and Lunch.")
+        global_error_stack.append("Start times must be lower than end times "
+            + "for WorkHours and Lunch.")
         return 0
     elif (meetDuration < 0 or meetDuration > 60):
-        global_error_stack.append("Meet duration must be in minutes and " +
-            "in the range of 0 to 60.\n" +
-            "Given value is: " + str(meetDuration))
+        global_error_stack.append("Meet duration must be in minutes and "
+            + "in the range of 0 to 60.\n"
+            + "Given value is: " + str(meetDuration))
         return 0
     else:
         # If all the values are okay then
         time = startTime
         while (time <= endTime):
             # Build the dictionary empty.
-            keyTime = deFortmatTime(time)
+            keyTime = DeformatTime(time)
             availableEmployees = [
                 employee
-                for employee in data["staff"]
-                if (isEmployeeAvailable(keyTime, data["staff"][employee]))
+                for employee in data['staff']
+                if (IsEmployeeAvailable(keyTime, data['staff'][employee]))
             ]
             if ((time < lunchStart or time > lunchEnd) and
                 (len(availableEmployees) > 2)):
@@ -181,12 +188,12 @@ def main(argv):
             time += meetDuration
 
         # Print result
-        printResults(availableTimes, inputfile)
+        PrintResults(availableTimes, inputfile)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
     # Check for errors.
     if (len(global_error_stack) > 0):
         print ("Errors found durring execution:")
         for msg in global_error_stack:
-            print("-> " + msg)
+            print("-> {}".format(msg))
